6dd1ee9409d3da8a48ea8b12f495e1569cb023e9
0x4D616E75 <0x4d616e75@elektronische-nachricht.de>
2019-09-13 23:30:46 +0200

    Add LinkQueryBuilder and LinkQueryOptions

---
+++ src/GitLabApiClient/Internal/Queries/LinkQueryBuilder.cs
@@ -0,0 +1,16 @@
+﻿using GitLabApiClient.Models.Releases.Requests;
+
+namespace GitLabApiClient.Internal.Queries
+{
+    class LinkQueryBuilder : QueryBuilder<LinkQueryOptions>
+    {
+        protected override void BuildCore(LinkQueryOptions options)
+        {
+            if (!string.IsNullOrEmpty(options.ProjectId))
+                Add("id", options.ProjectId);
+
+            if (!string.IsNullOrEmpty(options.TagName))
+                Add("tag_name", options.TagName);
+        }
+    }
+}
---
+++ src/GitLabApiClient/Models/Releases/Requests/LinkQueryOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace GitLabApiClient.Models.Releases.Requests
+{
+    public sealed class LinkQueryOptions
+    {
+        public string ProjectId { get; set; }
+        public string TagName { get; set; }
+
+        internal LinkQueryOptions(string projectId = null) => ProjectId = projectId;
+    }
+}
